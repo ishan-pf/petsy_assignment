51bc2fa082b89055b6592326f4c8cfc2c23ceb15
Ishan Gupta <55364798+ishan-pf@users.noreply.github.com>
2022-05-09 05:35:42 +0530

    Add files via upload

---
+++ Server/database/products/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+
+    title : {type: String , required : true},
+    description : { type : String , required : true },
+    image : { type : String , required : true },
+    categories : { type : Array  },
+    color : { type : Array  },
+    price : { type : Number , required : true },
+    inStock : { type : Boolean , default : true}
+},{
+    timestamps : true
+});
+
+export const ProductModel = mongoose.model("Products", ProductSchema);
